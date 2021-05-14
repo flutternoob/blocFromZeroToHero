f8e85f729a4f6a62b4b95f55a9db28376083df5e
flutternoob <62204476+flutternoob@users.noreply.github.com>
2021-05-14 21:41:30 +0530

    Update main.dart

--- #2 - Bloc Concepts - Streams, Blocs, Cubits/lib/main.dart
+++ #2 - Bloc Concepts - Streams, Blocs, Cubits/lib/main.dart
@@ -1,15 +1,21 @@
+void main(List<String args>) {
+  //Create a listenable stream to wait for items to arrive from the boatStream() function
+  Stream<int> stream = boatStream();
+  
+  //Listen to the stream to carry out the print command once the data has been received from the boatStream() function
+  stream.listen((receivedData) => print("Received boat number $receivedData"));
+}
+
+//This function generates the stream of data, in this case integers from 1 to 10
+//A stream always uses "async*" instead of async as used in futures
+//"*" indicates a generator function, i.e., a function which generates asynchronous data
+//Instead of "return", "yield" keyword is used to send back data to the calling object,
+//in this case the "stream" object in the main function
+
 Stream<int> boatStream() async* {
   for (int i = 1; i <= 10; i++) {
-    print("SENT boat no. " + i.toString());
+    print("Sent boat number $i");
     await Future.delayed(Duration(seconds: 2));
     yield i;
   }
-}
-
-void main(List<String> args) async {
-  Stream<int> stream = boatStream();
-
-  stream.listen((receivedData) {
-    print("RECEIVED boat no. " + receivedData.toString());
-  });
 }

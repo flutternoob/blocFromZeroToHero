11d9f63277f1c87e4c0061854dd98c963d4fd253
flutternoob <62204476+flutternoob@users.noreply.github.com>
2021-05-15 21:38:15 +0530

    Update counter_state.dart

--- #3 - Flutter Bloc Concepts/lib/cubit/counter_state.dart
+++ #3 - Flutter Bloc Concepts/lib/cubit/counter_state.dart
@@ -1,11 +1,8 @@
 part of 'counter_cubit.dart';
 
 class CounterState {
-  int counterValue;
-  bool wasIncremented;
+  int? counterValue;
+  bool? wasIncremented;
 
-  CounterState({
-    @required this.counterValue,
-    this.wasIncremented,
-  });
+  CounterState({required this.counterValue, this.wasIncremented});
 }

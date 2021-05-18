08cbdd8fc2dea6cd84cadc90f27076b4fa3e7b6d
flutternoob <62204476+flutternoob@users.noreply.github.com>
2021-05-18 15:56:35 +0530

    Update counter_cubit.dart

--- #5 - BLoC Testing/lib/cubit/counter_cubit.dart
+++ #5 - BLoC Testing/lib/cubit/counter_cubit.dart
@@ -1,6 +1,5 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:meta/meta.dart';
 
 part 'counter_state.dart';
 
@@ -8,8 +7,8 @@ class CounterCubit extends Cubit<CounterState> {
   CounterCubit() : super(CounterState(counterValue: 0));
 
   void increment() => emit(
-      CounterState(counterValue: state.counterValue + 1, wasIncremented: true));
+      CounterState(counterValue: state.counterValue! + 1, wasIncremented: true));
 
   void decrement() => emit(CounterState(
-      counterValue: state.counterValue - 1, wasIncremented: false));
+      counterValue: state.counterValue! - 1, wasIncremented: false));
 }

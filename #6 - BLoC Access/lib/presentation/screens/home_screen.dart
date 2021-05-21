1eb0b4b0fe78d177d53518f37b01bd0e85f873f3
flutternoob <62204476+flutternoob@users.noreply.github.com>
2021-05-21 18:43:38 +0530

    Update home_screen.dart

--- #6 - BLoC Access/lib/presentation/screens/home_screen.dart
+++ #6 - BLoC Access/lib/presentation/screens/home_screen.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_bloc_concepts/logic/cubit/counter_cubit.dart';
+import 'package:flutter_counter_bloc/logic/cubits/counter_cubit.dart';
+import 'package:flutter_counter_bloc/logic/functions/counter_functions.dart';
 
 class HomeScreen extends StatefulWidget {
-  HomeScreen({Key key, this.title, this.color}) : super(key: key);
+  final String? title;
+  final Color? color;
 
-  final String title;
-  final Color color;
+  const HomeScreen({Key? key, this.title, this.color}) : super(key: key);
 
   @override
   _HomeScreenState createState() => _HomeScreenState();
@@ -19,113 +20,48 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       key: homeScreenKey,
       appBar: AppBar(
-        backgroundColor: widget.color,
-        title: Text(widget.title),
+        title: Text(widget.title!),
+        backgroundColor: widget.color!,
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
+          children: [
+            Text("You have pushed this button this many times:"),
             BlocConsumer<CounterCubit, CounterState>(
-              listener: (context, state) {
-                if (state.wasIncremented == true) {
-                  homeScreenKey.currentState.showSnackBar(
-                    SnackBar(
-                      content: Text('Incremented!'),
-                      duration: Duration(milliseconds: 300),
-                    ),
-                  );
-                } else if (state.wasIncremented == false) {
-                  homeScreenKey.currentState.showSnackBar(
-                    SnackBar(
-                      content: Text('Decremented!'),
-                      duration: Duration(milliseconds: 300),
-                    ),
-                  );
-                }
-              },
-              builder: (context, state) {
-                if (state.counterValue < 0) {
-                  return Text(
-                    'BRR, NEGATIVE ' + state.counterValue.toString(),
-                    style: Theme.of(context).textTheme.headline4,
-                  );
-                } else if (state.counterValue % 2 == 0) {
-                  return Text(
-                    'YAAAY ' + state.counterValue.toString(),
-                    style: Theme.of(context).textTheme.headline4,
-                  );
-                } else if (state.counterValue == 5) {
-                  return Text(
-                    'HMM, NUMBER 5',
-                    style: Theme.of(context).textTheme.headline4,
-                  );
-                } else
-                  return Text(
-                    state.counterValue.toString(),
-                    style: Theme.of(context).textTheme.headline4,
-                  );
-              },
-            ),
-            SizedBox(
-              height: 24,
-            ),
+                listener: (context, state) => snackBarFunction(state.wasIncremented!, context),
+                builder: (context, state) => counterText(state.counterValue!, context)),
+            SizedBox(height: 24),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 FloatingActionButton(
-                  heroTag: Text('${widget.title}'),
-                  onPressed: () {
-                    BlocProvider.of<CounterCubit>(context).decrement();
-                    // context.bloc<CounterCubit>().decrement();
-                  },
-                  tooltip: 'Decrement',
+                  onPressed: () => BlocProvider.of<CounterCubit>(context).decrement(),
+                  backgroundColor: widget.color,
+                  heroTag: Text("${widget.title!}"),
+                  tooltip: "Decrement",
                   child: Icon(Icons.remove),
                 ),
                 FloatingActionButton(
-                  heroTag: Text('${widget.title} 2nd'),
-                  onPressed: () {
-                    // BlocProvider.of<CounterCubit>(context).increment();
-                    context.bloc<CounterCubit>().increment();
-                  },
-                  tooltip: 'Increment',
+                  onPressed: () => BlocProvider.of<CounterCubit>(context).increment(),
+                  backgroundColor: widget.color,
+                  heroTag: Text("${widget.title!} #2"),
+                  tooltip: "Increment",
                   child: Icon(Icons.add),
                 ),
               ],
             ),
-            SizedBox(
-              height: 24,
-            ),
+            SizedBox(height: 24),
             MaterialButton(
+              onPressed: () => Navigator.of(context).pushNamed('/second'),
               color: Colors.redAccent,
-              child: Text(
-                'Go to Second Screen',
-                style: TextStyle(color: Colors.white),
-              ),
-              onPressed: () {
-                Navigator.of(context).pushNamed(
-                  '/second',
-                  arguments: homeScreenKey,
-                );
-              },
+              child: Text("Go to Second Screen", style: TextStyle(color: Colors.white)),
             ),
-            SizedBox(
-              height: 24,
-            ),
+            SizedBox(height: 24),
             MaterialButton(
+              onPressed: () => Navigator.of(context).pushNamed('/third'),
               color: Colors.greenAccent,
-              child: Text(
-                'Go to Third Screen',
-                style: TextStyle(color: Colors.white),
-              ),
-              onPressed: () {
-                Navigator.of(context).pushNamed(
-                  '/third',
-                );
-              },
+              child: Text("Go to Third Screen", style: TextStyle(color: Colors.white)),
             ),
           ],
         ),

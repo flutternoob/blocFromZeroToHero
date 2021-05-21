91f4f1e46ede5eadd678849cfcbbf73fe1751e8c
flutternoob <62204476+flutternoob@users.noreply.github.com>
2021-05-21 18:44:11 +0530

    Update second_screen.dart

--- #6 - BLoC Access/lib/presentation/screens/second_screen.dart
+++ #6 - BLoC Access/lib/presentation/screens/second_screen.dart
@@ -1,124 +1,59 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_bloc_concepts/logic/cubit/counter_cubit.dart';
+import 'package:flutter_counter_bloc/logic/cubits/counter_cubit.dart';
+import 'package:flutter_counter_bloc/logic/functions/counter_functions.dart';
 
 class SecondScreen extends StatefulWidget {
-  SecondScreen({
-    Key key,
-    this.title,
-    this.color,
-    this.homeScreenKey,
-  }) : super(key: key);
+  final String? title;
+  final Color? color;
 
-  final String title;
-  final Color color;
-  final GlobalKey<ScaffoldState> homeScreenKey;
+  const SecondScreen({Key? key, this.title, this.color}) : super(key: key);
 
   @override
   _SecondScreenState createState() => _SecondScreenState();
 }
 
 class _SecondScreenState extends State<SecondScreen> {
-  GlobalKey<ScaffoldState> secondScreenKey = GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      key: secondScreenKey,
       appBar: AppBar(
-        backgroundColor: widget.color,
-        title: Text(widget.title),
+        title: Text(widget.title!),
+        backgroundColor: widget.color!,
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
+          children: [
+            Text("You have pushed this button this many times:"),
             BlocConsumer<CounterCubit, CounterState>(
-              listener: (context, state) {
-                if (state.wasIncremented == true) {
-                  widget.homeScreenKey.currentState.removeCurrentSnackBar();
-                  secondScreenKey.currentState.showSnackBar(
-                    SnackBar(
-                      content: Text('Incremented!'),
-                      duration: Duration(milliseconds: 300),
-                    ),
-                  );
-                } else if (state.wasIncremented == false) {
-                  widget.homeScreenKey.currentState.removeCurrentSnackBar();
-                  secondScreenKey.currentState.removeCurrentSnackBar();
-                  Scaffold.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text('Decremented!'),
-                      duration: Duration(milliseconds: 300),
-                    ),
-                  );
-                }
-              },
-              builder: (context, state) {
-                if (state.counterValue < 0) {
-                  return Text(
-                    'BRR, NEGATIVE ' + state.counterValue.toString(),
-                    style: Theme.of(context).textTheme.headline4,
-                  );
-                } else if (state.counterValue % 2 == 0) {
-                  return Text(
-                    'YAAAY ' + state.counterValue.toString(),
-                    style: Theme.of(context).textTheme.headline4,
-                  );
-                } else if (state.counterValue == 5) {
-                  return Text(
-                    'HMM, NUMBER 5',
-                    style: Theme.of(context).textTheme.headline4,
-                  );
-                } else
-                  return Text(
-                    state.counterValue.toString(),
-                    style: Theme.of(context).textTheme.headline4,
-                  );
-              },
-            ),
-            SizedBox(
-              height: 24,
-            ),
+                listener: (context, state) => snackBarFunction(state.wasIncremented!, context),
+                builder: (context, state) => counterText(state.counterValue!, context)),
+            SizedBox(height: 24),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 FloatingActionButton(
-                  heroTag: Text('${widget.title}'),
+                  onPressed: () => BlocProvider.of<CounterCubit>(context).decrement(),
                   backgroundColor: widget.color,
-                  onPressed: () {
-                    BlocProvider.of<CounterCubit>(context).decrement();
-                    // context.bloc<CounterCubit>().decrement();
-                  },
-                  tooltip: 'Decrement',
+                  heroTag: Text("${widget.title!}"),
+                  tooltip: "Decrement",
                   child: Icon(Icons.remove),
                 ),
                 FloatingActionButton(
+                  onPressed: () => BlocProvider.of<CounterCubit>(context).increment(),
                   backgroundColor: widget.color,
-                  heroTag: Text('${widget.title} 2nd'),
-                  onPressed: () {
-                    // BlocProvider.of<CounterCubit>(context).increment();
-                    context.bloc<CounterCubit>().increment();
-                  },
-                  tooltip: 'Increment',
+                  heroTag: Text("${widget.title!} #2"),
+                  tooltip: "Increment",
                   child: Icon(Icons.add),
                 ),
               ],
             ),
-            SizedBox(
-              height: 24,
-            ),
+            SizedBox(height: 24),
             MaterialButton(
+              onPressed: () => Navigator.of(context).pushNamed('/third'),
               color: Colors.greenAccent,
-              child: Text(
-                'Go to Third Screen',
-                style: TextStyle(color: Colors.white),
-              ),
-              onPressed: () {
-                Navigator.of(context).pushNamed('/third');
-              },
+              child: Text("Go to Third Screen", style: TextStyle(color: Colors.white)),
             ),
           ],
         ),

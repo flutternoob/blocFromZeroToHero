2c67b154ca994748348499a6a1b3c41909897a9b
flutternoob <62204476+flutternoob@users.noreply.github.com>
2021-05-25 20:43:40 +0530

    Update app_router.dart

--- #7 - BLoC Communication/lib/presentation/router/app_router.dart
+++ #7 - BLoC Communication/lib/presentation/router/app_router.dart
@@ -1,33 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_bloc_concepts/logic/cubit/counter_cubit.dart';
-import 'package:flutter_bloc_concepts/presentation/screens/home_screen.dart';
-import 'package:flutter_bloc_concepts/presentation/screens/second_screen.dart';
-import 'package:flutter_bloc_concepts/presentation/screens/third_screen.dart';
+import 'package:flutter_counter_bloc/presentation/screens/home_screen.dart';
+import 'package:flutter_counter_bloc/presentation/screens/second_screen.dart';
+import 'package:flutter_counter_bloc/presentation/screens/third_screen.dart';
 
 class AppRouter {
-  Route onGenerateRoute(RouteSettings settings) {
-    switch (settings.name) {
+  Route? onGeneratedRoute(RouteSettings? routeSettings) {
+    switch (routeSettings!.name) {
       case '/':
         return MaterialPageRoute(
-          builder: (_) => HomeScreen(
-            title: "Home Screen",
-            color: Colors.blueAccent,
-          ),
+            builder: (_) => HomeScreen(
+              title: "Home Screen",
+              color: Colors.blueAccent,
+            )
         );
       case '/second':
         return MaterialPageRoute(
-          builder: (_) => SecondScreen(
-            title: "Second Screen",
-            color: Colors.redAccent,
-          ),
+            builder: (_) => SecondScreen(
+              title: "Second Screen",
+              color: Colors.redAccent,
+            )
         );
       case '/third':
         return MaterialPageRoute(
-          builder: (_) => ThirdScreen(
-            title: "Thirst Screen",
-            color: Colors.greenAccent,
-          ),
+            builder: (_) => ThirdScreen(
+              title: "Third Screen",
+              color: Colors.greenAccent,
+            )
         );
       default:
         return null;

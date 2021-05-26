0d27bd47bf72a2db97e6ac4afa470d0097a36987
flutternoob <62204476+flutternoob@users.noreply.github.com>
2021-05-26 15:12:52 +0530

    Update counter_cubit.dart

--- #8 - BuildContexts/lib/logic/cubit/counter_cubit.dart
+++ #8 - BuildContexts/lib/logic/cubit/counter_cubit.dart
@@ -1,15 +1,39 @@
-import 'package:bloc/bloc.dart';
+import 'dart:async';
 
-import 'package:meta/meta.dart';
+import 'package:bloc/bloc.dart';
+import 'internet_cubit.dart';
+import 'package:flutter_counter_bloc/constants/enums.dart';
 
 part 'counter_state.dart';
 
 class CounterCubit extends Cubit<CounterState> {
-  CounterCubit() : super(CounterState(counterValue: 0));
+  final InternetCubit internetCubit;
+  StreamSubscription? internetStreamSubscription;
+
+  CounterCubit({required this.internetCubit}) : super(CounterState(counterValue: 0)){
+   monitorInternetCubit();
+  }
+
+  void monitorInternetCubit() {
+    internetStreamSubscription = internetCubit.stream.listen((internetState) {
+      if (internetState is InternetConnected && internetState.connectionType == ConnectionType.Wifi){
+        increment();
+      }
+      else if (internetState is InternetConnected && internetState.connectionType == ConnectionType.Mobile){
+        decrement();
+      }
+    });
+  }
 
   void increment() => emit(
-      CounterState(counterValue: state.counterValue + 1, wasIncremented: true));
+      CounterState(counterValue: state.counterValue! + 1, wasIncremented: true));
 
   void decrement() => emit(CounterState(
-      counterValue: state.counterValue - 1, wasIncremented: false));
+      counterValue: state.counterValue! - 1, wasIncremented: false));
+
+  @override
+  Future<void> close() async {
+    internetStreamSubscription!.cancel();
+    super.close();
+  }
 }

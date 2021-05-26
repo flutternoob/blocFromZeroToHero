f533822a504018b3eb8921ba3addb1f896538b03
flutternoob <62204476+flutternoob@users.noreply.github.com>
2021-05-26 15:19:07 +0530

    Delete counter_state.dart

--- #8 - BuildContexts/lib/logic/cubit/counter_state.dart
+++
@@ -1,8 +0,0 @@
-part of 'counter_cubit.dart';
-
-class CounterState {
-  final int? counterValue;
-  final bool? wasIncremented;
-
-  CounterState({required this.counterValue, this.wasIncremented});
-}
